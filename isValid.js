793791cce3bbd12cfa0e343d06674484aa5f2e69
DeAndre Boston <dboston3@yahoo.com>
2020-10-19 23:12:35 -0500

    add isValidIp function

---
+++ isValid.js
@@ -0,0 +1,22 @@
+function isValidIP(str) {
+    if (str === ''){
+      return false
+    }
+    let split = str.split(".")
+    if (split.length > 4 || split.length < 0){
+      return false
+    } else {
+      split.forEach(function(item){
+        console.log(parseInt(item))
+        if (parseInt(item)){
+            return false
+          }
+        if(parseInt(item) > 255 || parseInt(item) < 0) {
+          return false
+        }
+      })
+    }
+    return true;
+  }
+
+  console.log(isValidIP('abc.def.ghi.jkl'))
